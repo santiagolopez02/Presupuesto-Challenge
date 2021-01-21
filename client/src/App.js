cd0bddde6c3847db40650b851215d8c479250088
santiago lopez <lopezsantiago35699@gmail.com>
2021-01-21 11:59:01 -0300

    Create Reacti App

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+      <h1>Hola App</h1>
+  );
+}
+
+export default App;
